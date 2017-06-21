a88f10cb4b8b4403c7606b8c93414f8292bfc86a
EMILY <stmartin.emily@gmail.com>
2017-06-21 10:33:24 -0500

    Add shop by category

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,8 +1,16 @@
 class StorefrontController < ApplicationController
   def index
+    if params[:cat_id]
+      # I only want to see the stuff in that category
+      @category = Category.find(params[:cat_id])
+      @products = Product.where(category_id: params[:cat_id])
+    else 
     @products = Product.all 
   end
+   end
 
   def about
   end
+
+
 end
